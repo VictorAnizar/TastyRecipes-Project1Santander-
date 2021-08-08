f7057843dec97fc654411634c53b409042fc2d3e
SDWarkXD <sdwark0925@gmail.com>
2021-08-08 12:54:21 -0500

    carga de imagenes desde html final

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
             },
             {
                 test: /\.(png|jpg|gif)$/i,
-                use: [ "file-loader" ]
+                type: 'asset/resource'
             }
         ]
     },
