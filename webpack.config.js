511614cd08ea362fc58d152f910aecd88b6c4038
VictorAnizar <victoranizarmorales@gmail.com>
2021-08-07 21:30:57 -0500

    subiendo babel

--- webpack.config.js
+++ webpack.config.js
@@ -39,15 +39,7 @@ module.exports = {
             },
             {
                 test: /\.(png|jpg|gif)$/i,
-                use: [
-                  {
-                    loader: 'url-loader',
-                    options: {
-                      limit: 8192,
-                    }
-                  },
-                ],
-               type: 'javascript/auto'
+                use: [ "file-loader" ]
             }
         ]
     },
