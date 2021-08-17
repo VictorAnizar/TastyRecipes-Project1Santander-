f2b19f934a4974cf3a7820d114efa31877dfd476
daahou <andrechapina51@aragon.unam.mx>
2021-08-17 15:57:43 -0500

    Last changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin') 
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/js/index.js',//lee el archivo js
+    entry: "./src/js/index.js", //lee el archivo js
     output: {
-        path: path.resolve(__dirname, 'docs'),//construye esta carpeta 
-        filename: 'bundle.js',//arcchivo empaquetado
-        assetModuleFilename: 'assets/[hash][ext][query]'
-    }, 
-    plugins: [ //se usa un plugin para leer html
+        path: path.resolve(__dirname, "docs"), //construye esta carpeta
+        filename: "bundle.js", //arcchivo empaquetado
+        assetModuleFilename: "assets/[hash][ext][query]",
+    },
+    plugins: [
+        //se usa un plugin para leer html
         new HtmlWebpackPlugin({
-            filename: 'index.html',//toma este archivo 
-            template: './src/index.html'//y genera un archivo al final
-        })
+            filename: "index.html", //toma este archivo
+            template: "./src/index.html", //y genera un archivo al final
+        }),
     ],
-    module:{
-        rules:[
+    module: {
+        rules: [
             {
-                test: /\.s[ac]ss$/i,//cuando pasa la prueba
+                test: /\.s[ac]ss$/i, //cuando pasa la prueba
                 use: [
                     // Creates `style` nodes from JS strings
                     "style-loader",
@@ -25,33 +26,32 @@ module.exports = {
                     "css-loader",
                     // Compiles Sass to CSS
                     "sass-loader",
-                  ]//usa estos loaders
+                ], //usa estos loaders
             },
             {
                 test: /\.svg/,
                 use: {
-                  loader: "svg-url-loader",
-                  options: {},
+                    loader: "svg-url-loader",
+                    options: {},
                 },
             },
             {
                 test: /\.m?js$/,
                 exclude: /node_modules/,
                 use: {
-                  loader: "babel-loader",
-                  options: {
-                    presets: ['@babel/preset-env']
-                  }
-                }
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
+                },
             },
             {
                 test: /\.(png|jpg|gif)$/i,
-                use: [ "file-loader" ]
-            }
-        ]
+                use: ["file-loader"],
+            },
+        ],
     },
     devServer: {
-        contentBase: path.resolve(__dirname, 'dist')
+        contentBase: path.resolve(__dirname, "dist"),
     },
-    
-}
+};
